6c3c59c8b339654dc22c0587f068f59fb28394c3
thunderbikes <54989456+thunderbikes@users.noreply.github.com>
2022-05-19 20:33:34 -0600

    Delete Currentvstime.m

--- Currentvstime.m
+++
@@ -1,7 +0,0 @@
-plot(energy_time,(energy*1.05/100.8))
-hold on
-title("Current as a Function of Time");
-xlabel("Time (s)");
-ylabel("Current Used (A)");
-Q = trapz(energy_time,(energy*1.05/100.8));
-disp("capacity (Ah): "+ (Q));
